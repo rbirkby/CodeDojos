fe353bc3ea5ea7c678db694bf118d4419bcc1990
Richard Birkby <rbirkby@gmail.com>
2014-11-20 21:41:06 +0000

    Implement stories 1 & 2

---
+++ BankOcrBootcamp/PartialSolutionFromBootcamp/TddOneOhOne/BankOcrTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TddOneOhOne
+{
+    [TestFixture]
+    public class BankOcrTests
+    {
+        [Test]
+        public void GivenMalformedInput_ThenShouldThrowException()
+        {
+            const string input = " _ \n" +
+                                 "| |";
+
+            var target = new Ocr();
+            Assert.Throws<MalformedDigitsException>(() => target.Recognize(input));
+        }
+
+        [Test]
+        public void GivenMalformedLines_ThenShouldThrowException()
+        {
+            const string input = "    _  _     _  _  _  _  _ \n" +
+                                 "  | _| _||_||_ |_   ||_||_|\n" +
+                                 "  ||_  _|  | _||_||\n";
+
+            var target = new Ocr();
+            Assert.Throws<MalformedDigitsException>(() => target.Recognize(input));
+        }
+
+        [Test]
+        public void GivenUnrecognizedDigit_ThenShouldThrowException()
+        {
+            const string input = "   \n" +
+                                 "|_|\n" +
+                                 "| |\n";
+
+            var target = new Ocr();
+            Assert.Throws<MalformedDigitsException>(() => target.Recognize(input));
+        }
+
+        [Test]
+        public void GivenAllDigits_ThenAllRecognizedDigitsReturned()
+        {
+            const string input = "    _  _     _  _  _  _  _ \n" +
+                                 "  | _| _||_||_ |_   ||_||_|\n" +
+                                 "  ||_  _|  | _||_|  ||_| _|\n";
+
+            var target = new Ocr();
+            Assert.That(target.Recognize(input), Is.EquivalentTo(Enumerable.Range(1, 9)));
+        }
+    }
+}
---
+++ BankOcrBootcamp/PartialSolutionFromBootcamp/TddOneOhOne/Ocr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TddOneOhOne
+{
+    public class Ocr
+    {
+        public IEnumerable<int> Recognize(string input)
+        {
+            var rows = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (rows.Length != 3)
+                throw new MalformedDigitsException();
+
+            var row1Groups = rows.ElementAt(0).Buffer(3);
+            var row2Groups = rows.ElementAt(1).Buffer(3);
+            var row3Groups = rows.ElementAt(2).Buffer(3);
+
+            return (from sevenSegments in row1Groups.Zip(row2Groups.Zip(row3Groups, Tuple.Create), (a, b) => a.Concat(b.Item1).Concat(b.Item2))
+                    select Parse(new string(sevenSegments.ToArray()))).ToList();
+        }
+
+        private int Parse(string sevenSegments)
+        {
+            switch (sevenSegments)
+            {
+                case " _ | ||_|": return 0;
+                case "     |  |": return 1;
+                case " _  _||_ ": return 2;
+                case " _  _| _|": return 3;
+                case "   |_|  |": return 4;
+                case " _ |_  _|": return 5;
+                case " _ |_ |_|": return 6;
+                case " _   |  |": return 7;
+                case " _ |_||_|": return 8;
+                case " _ |_| _|": return 9;
+                default:
+                    throw new MalformedDigitsException();
+            }
+        }
+    }
+}
